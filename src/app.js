0ea12d9c4618bffb90df88046169248b58b3012c
Daniel Nuñez PC <danyelalo31@gmail.com>
2022-11-21 16:09:40 -0600

    Prueba de tooltip completada

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
-let stop = document.getElementById("looped_animation");
-looped_animation.addEventListener("mouseover", function () {
+
+let stopped = document.getElementById("looped_animation");
+stopped.addEventListener("mouseover", function () {
     document.getElementById("looped_animation").className = "navbar-brand animate__animated animate__shakeY"
 })
 let move = document.getElementById("looped_animation");
-looped_animation.addEventListener("mouseout", function () {
-    document.getElementById("looped_animation").className = "navbar-brand animate__animated animate__shakeY animate__slower animate__infinite	infinite"
+move.addEventListener("mouseout", function () {
+    document.getElementById("looped_animation").className = "navbar-brand animate__animated animate__shakeY animate__slower animate__infinite infinite"
+})
+let especTooltip = document.getElementById("espec");
+especTooltip.addEventListener("mouseenter",() => {
+    document.getElementById("espec-tooltip").classList.remove("invisible");
+})
+especTooltip.addEventListener("mouseleave",() => {
+    document.getElementById("espec-tooltip").classList.add("invisible");    
 })
