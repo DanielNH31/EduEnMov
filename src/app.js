e6f43f47a7478ad270209a6e1ea181c0efd8b375
Daniel Nuñez <danyelalo31@gmail.com>
2022-11-25 08:28:57 -0600

    UI corrections

--- src/app.js
+++ src/app.js
@@ -7,24 +7,3 @@ let move = document.getElementById("looped_animation");
 move.addEventListener("mouseout", function () {
     document.getElementById("looped_animation").className = "navbar-brand animate__animated animate__shakeY animate__slower animate__infinite infinite"
 })
-let especTooltip = document.getElementById("espec");
-especTooltip.addEventListener("mouseenter",() => {
-    document.getElementById("espec-tooltip").classList.remove("invisible");
-})
-especTooltip.addEventListener("mouseleave",() => {
-    document.getElementById("espec-tooltip").classList.add("invisible");    
-})
-let critTooltip = document.getElementById("crit");
-critTooltip.addEventListener("mouseenter",() => {
-    document.getElementById("crit-tooltip").classList.remove("invisible");
-})
-critTooltip.addEventListener("mouseleave",() => {
-    document.getElementById("crit-tooltip").classList.add("invisible");
-})
-let defTooltip = document.getElementById("def");
-defTooltip.addEventListener("mouseenter",() => {
-    document.getElementById("def-tooltip").classList.remove("invisible");
-})
-defTooltip.addEventListener("mouseleave",() => {
-    document.getElementById("def-tooltip").classList.add("invisible");
-})
